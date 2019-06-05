c9a9c876d345ed76be506e961dc6620bc4a04248
Tom Carabine <Tom.Carabine@kpmg.co.uk>
2019-06-05 15:24:53 +0000

    better output

--- src/main.go
+++ src/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"time"
+	"flag"
 )
 
 type node struct {
@@ -13,10 +14,15 @@ type node struct {
 
 func main () {
 	tStart := time.Now()
-
+	
 	dictionaryPath := "/workspaces/Word-Ladder/data/dictionary.txt"
-	startWord := "cat"
-	endWord := "dog"
+
+	var startWord,endWord string
+	flag.StringVar(&startWord, "start", "sport", "Word to start search from")
+	flag.StringVar(&endWord, "end", "crate", "Word to end on")
+
+	flag.Parse()
+
 	length := len(startWord)
 
 	dictionary := load(dictionaryPath, length)
@@ -29,7 +35,20 @@ func main () {
 	path := bfs(dictionary,startWord,endWord)
 
 	fmt.Println("Got there in ", len(path))
-	fmt.Printf("%v\n", path)
+
+	for i := 0; i < len(path)/2; i++ {
+		j := len(path) - i - 1
+		path[i], path[j] = path[j], path[i]
+	}
+
+	for p := range path {
+		if p < len(path)-1 {
+			fmt.Printf("%v -> ", path[p])
+		} else {
+			fmt.Printf("%v \n", path[p])
+		}
+	}
+
 	tElapsedBFS := time.Since(tStartBFS)
 	fmt.Println("Found path in ",tElapsedBFS.String())
 	tTotal := time.Since(tStart)
